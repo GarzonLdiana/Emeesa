d7fa83d485fc50ce8b8814b01e557c63fc684ac4
Pagove78 <pagove78@gmail.com>
2024-05-24 20:18:35 -0500

    Update tarifas.vista.php

--- Vistas/Tarifas/tarifas.vista.php
+++ Vistas/Tarifas/tarifas.vista.php
@@ -21,20 +21,185 @@
      <table class="table">
             <thead>
                 <tr>
-                    <th>Fecha Desde</th>
-                    <th>Fecha Hasta</th>
-                    <th>Descargar</th>
+
+                <br>
+        <h2>Tarifas Año 2024 </h2>
+        <br>
+           <br>
+                    
+                
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td><input type="date" id="start" name="trip-start" value="2018-07-22" min="2018-01-01" max="2023-12-31" /></td>
-                    <td><input type="date" id="start" name="trip-start" value="2018-07-22" min="2018-01-01" max="2023-12-31" /></td>
-                    <td><a href="factura_001.pdf" target="_blank">Descargar</a></td>
-                    <td><button onclick="consultarFactura('factura_001.pdf')">Consultar</button></td>
+                                  
+                <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar">ABRIL</button>
+                    </th>
+                    
+                 
+                <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar"> MARZO </button>
+                </th>
+                   
+                               
+                <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar"> FEBRERO </button>
+                </th>
+
+                                
+                    <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar"> ENERO </button>
+                    
+                </th>
+                
                 </tr>
+               
             </tbody>
+
+              
         </table>
+        
+        <div class="card-body">
+        <table class="table">
+        <thead>
+                <tr>
+
+                <br>
+        <h2>Tarifas Año 2023 </h2>
+        <br>
+           <br>
+                    
+                
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                                  
+                <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar">DICIEMBRE</button>
+                    </th>
+                    
+                 
+                <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar"> NOVIEMBRE </button>
+                </th>
+                   
+                               
+                <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar"> OCTUBRE </button>
+                </th>
+
+                                
+                    <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar"> SEPTIEMBRE </button>
+                    
+                </th>
+                
+                </tr>
+        </tbody>
+
+        <tbody>
+                <tr>
+                                  
+                <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar"> AGOSTO </button>
+                    </th>
+                    
+                 
+                <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar"> JULIO </button>
+                </th>
+                   
+                               
+                <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar"> JUNIO </button>
+                </th>
+
+                                
+                    <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar"> MAYO </button>
+                    
+                </th>
+                
+                </tr>
+
+                
+        </tbody>
+        <tbody>
+                <tr>
+                                  
+                <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar"> ABRIL </button>
+                    </th>
+                    
+                 
+                <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar"> MARZO </button>
+                </th>
+                   
+                               
+                <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar"> FEBRERO </button>
+                </th>
+
+                                
+                    <th style="text-align: center; border: 1px solid #f7eeee; padding: 10px;">
+                    <img src="./assets/imagenes/tarifa.png" alt="Icono de Tarifa"
+                        style="width: 80px; display: block; margin: 0 auto;">
+                    <br>Tarifa
+                    <br> <button onclick="consultar"> ENERO </button>
+                    
+                </th>
+                
+                </tr>
+        </tbody>
+        </th>
     </div>
         <!-- Resto de las secciones -->
     </section>
@@ -51,6 +216,8 @@
 </script>
 
 <!-- Agrega la referencia a Bootstrap JS y jQuery -->
+<div class="card-body">
+        <table class="table">
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
