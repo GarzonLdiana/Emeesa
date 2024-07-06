73b6bc27735081c110bb37b1f025d6b2e0461275
GarzonLdiana <166877859+GarzonLdiana@users.noreply.github.com>
2024-07-06 07:59:36 -0500

    actualizacion vista verificaciones

--- Vistas/Consumos/Verificacion/verificacion.vista.php
+++ Vistas/Consumos/Verificacion/verificacion.vista.php
@@ -2,7 +2,6 @@
 require_once "./controladores/Verificacion/verificacion.controller.php";
 ?>
 
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -45,47 +44,57 @@ require_once "./controladores/Verificacion/verificacion.controller.php";
           </div>
         </div>
         <div class="card-body">
-          <p>En esta sección, podrás consultar las lecturas del medidor de los ultimos 12 meses:</p>
+          <p>En esta sección, podrás ingresar, consultar las lecturas del medidor de los últimos 12 meses:</p>
           <div class="row">
             <div class="col-md-12">
-              <!-- advanced Tables --> 
-              <div class="panel panel-default">
-                <div class="panel-body">
-                  <div class="table-responsive">
-                    <table id="datatables" class="table table-sm table-striped table-bordered table-hover datatable">          
-                      <thead class="table-header">
-                        <tr>
-                          <th width="15%">Número de Factura</th>
-                          <th width="15%">Fecha de Lecturas</th>
-                          <th width="15%">lectura del Medidor</th>
-                          <th width="30%">Foto del Medidor</th>
-                          <th width="15%">Descargar</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                      <?php
-                        // Llamar al controlador para recuperar los registros de la tabla de base de datos
-                        $Verificacion = VerificacionController::index();
-                        foreach($Verificacion as $key => $Verificaciones){
-                            echo '<tr>
-                                    <td>'.$Verificaciones["id_lectura"].'</td>
-                                    <td>'.$Verificaciones["fecha_lectura"].'</td>
-                                    <td>'.$Verificaciones["detalle_lectura"].'</td>
-                                    <td>'.$Verificaciones["registro_fotografico"].'</td>';
-                            
-                            echo '<td>
-                                    <a href="#" class="btn btn-warning btn-sm">
-                                    <i class="fa fa-download nav-icon"></i> <span>PDF</span>
-                                    </a>
-                                </td>
-                            </tr>';
-                        }
-                      ?>
-                      </tbody>
-                    </table>
-                    <div id="verificacion-consulta" style="display: none;">
-                      <h3>Lectura Consultada</h3>
-                      <iframe id="verificacion-iframe" width="100%" height="500" frameborder="0"></iframe>
+              <!-- Botón Añadir -->
+              <div class="mb-3">
+                <a href="#" class="btn btn-success" onclick="abrirFormulario()">
+                  <i class="fa fa-plus"></i> Generar solicitud
+                </a>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <!-- advanced Tables --> 
+                  <div class="panel panel-default">
+                    <div class="panel-body">
+                      <div class="table-responsive">
+                        <table id="datatables" class="table table-sm table-striped table-bordered table-hover datatable">          
+                          <thead class="table-header">
+                            <tr>
+                              <th width="15%">Número de Factura</th>
+                              <th width="15%">Fecha de Lectura</th>
+                              <th width="15%">Lectura del Medidor</th>
+                              <th width="20%">Foto del Medidor</th>
+                              <th width="15%">Descargar</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                          <?php
+                            // Llamar al controlador para recuperar los registros de la tabla de base de datos
+                            $Verificacion = VerificacionController::index();
+                            foreach($Verificacion as $key => $Verificaciones){
+                                echo '<tr>
+                                        <td>'.$Verificaciones["id_lectura"].'</td>
+                                        <td>'.$Verificaciones["fecha_lectura"].'</td>
+                                        <td>'.$Verificaciones["detalle_lectura"].'</td>
+                                        <td>'.$Verificaciones["registro_fotografico"].'</td>';
+                                
+                                echo '<td>
+                                        <a href="#" class="btn btn-warning btn-sm">
+                                        <i class="fa fa-download nav-icon"></i> <span>PDF</span>
+                                        </a>
+                                    </td>
+                                </tr>';
+                            }
+                          ?>
+                          </tbody>
+                        </table>
+                        <div id="verificacion-consulta" style="display: none;">
+                          <h3>Lectura Consultada</h3>
+                          <iframe id="verificacion-iframe" width="100%" height="500" frameborder="0"></iframe>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -116,19 +125,18 @@ require_once "./controladores/Verificacion/verificacion.controller.php";
   <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
   <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.print.min.js"></script>
 
-  <!-- Incluye  script  -->
+  <!-- Incluye script -->
   <script src="plantilla.js"></script>
 
   <script>
     //$(document).ready(function() {
-   //     $('#datatables').DataTable({
+    //     $('#datatables').DataTable({
     //        // configuración adicionales para DataTables
-   //     });
-   // });
+    //     });
+    // });
 
     function consultarVerificacion(rutaVerificacion) {
       // Mostrar el área de consulta y cargar la factura en el iframe
-
       const verificacionConsulta = document.getElementById('verificacion-consulta');
       const verificacionIframe = document.getElementById('verificacion-iframe');
       verificacionIframe.src = rutaVerificacion;
